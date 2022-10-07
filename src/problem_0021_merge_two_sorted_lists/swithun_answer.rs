3a345231ab475f405520441cf5b96ce48b83ecc7
swithun <2571021108@qq.com>
2022-10-07 18:28:51 +0800

    rust 21

---
+++ src/problem_0021_merge_two_sorted_lists/swithun_answer.rs
@@ -0,0 +1,64 @@
+use crate::data_structures::ListNode;
+
+pub struct Solution;
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+use std::mem;
+
+impl Solution {
+    pub fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut x = Some(2);
+        match x.as_mut() {
+            Some(v) => *v = 42,
+            None => {}
+        }
+
+        let (mut l1, mut l2) = (l1, l2);
+        let mut result = None;
+        let mut target = &mut result;
+
+        loop {
+            match (&mut l1, &mut l2) {
+                (None, None) => {
+                    break;
+                }
+                (None, rest @ Some(_)) | (rest @ Some(_), None) => {
+                    *target = rest.take();
+                    break;
+                }
+                (Some(left), Some(right)) => {
+                    *target = if left.val <= right.val {
+                        let new_l1 = left.next.take();
+
+                        mem::replace(&mut l1, new_l1)
+                    } else {
+                        let new_l2 = right.next.take();
+
+                        mem::replace(&mut l2, new_l2)
+                    };
+
+                    target = &mut target.as_mut().unwrap().next;
+                }
+            }
+        }
+
+        result
+    }
+}
+
+// ------------------------------------------------------ snip ------------------------------------------------------ //
+
+impl super::Solution for Solution {
+    fn merge_two_lists(l1: Option<Box<ListNode>>, l2: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        Self::merge_two_lists(l1, l2)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_solution() {
+        super::super::tests::run::<super::Solution>();
+    }
+}
